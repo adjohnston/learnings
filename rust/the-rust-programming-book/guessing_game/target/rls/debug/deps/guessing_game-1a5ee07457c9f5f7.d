ffcdff3a40cdc67c2386b8fc8c9f55c804bff062
adjohnston <adam.johnston3679@gmail.com>
2019-06-08 17:46:47 +0100

    compiled from new

---
+++ rust/the-rust-programming-book/guessing_game/target/rls/debug/deps/guessing_game-1a5ee07457c9f5f7.d
@@ -0,0 +1,5 @@
+/Users/adam/Sites/learnings/rust/the-rust-programming-book/guessing_game/target/rls/debug/deps/guessing_game-1a5ee07457c9f5f7.rmeta: src/main.rs
+
+/Users/adam/Sites/learnings/rust/the-rust-programming-book/guessing_game/target/rls/debug/deps/guessing_game-1a5ee07457c9f5f7.d: src/main.rs
+
+src/main.rs:
