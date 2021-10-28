a3b6a415d6f7c10777a46970b8153c9d0482d343
Adam Johnston <666676+adjohnston@users.noreply.github.com>
2021-10-28 16:01:18 +0100

    Formatting

--- typescript/fp-ts-reader-task-either.ts
+++ typescript/fp-ts-reader-task-either.ts
@@ -1,53 +1,53 @@
-import * as TE from "fp-ts/TaskEither";
-import * as RTE from "fp-ts/ReaderTaskEither";
-import * as R from "fp-ts/Reader";
-import * as RE from "fp-ts/ReaderEither";
-import * as E from "fp-ts/Either";
-import { pipe } from "fp-ts/function";
+import * as TE from 'fp-ts/TaskEither'
+import * as RTE from 'fp-ts/ReaderTaskEither'
+import * as R from 'fp-ts/Reader'
+import * as RE from 'fp-ts/ReaderEither'
+import * as E from 'fp-ts/Either'
+import { pipe } from 'fp-ts/function'
 
 interface User {
-  id: number;
-  name: string;
-  email: string;
+  id: number
+  name: string
+  email: string
 }
 
 interface Item {
-  id: number;
-  name: string;
-  qty: number;
-  unitPrice: number;
+  id: number
+  name: string
+  qty: number
+  unitPrice: number
 }
 
 interface Basket {
-  id: number;
-  userId: number;
-  items: Item[];
+  id: number
+  userId: number
+  items: Item[]
 }
 
 interface Config {
-  code: "gb" | "no";
-  name: "United Kingdom" | "Norway";
-  vatRate: number;
+  code: 'gb' | 'no'
+  name: 'United Kingdom' | 'Norway'
+  vatRate: number
 }
 
 const gbConfig: Config = {
-  code: "gb",
-  name: "United Kingdom",
+  code: 'gb',
+  name: 'United Kingdom',
   vatRate: 0.2,
-};
+}
 
 const noConfig: Config = {
-  code: "no",
-  name: "Norway",
+  code: 'no',
+  name: 'Norway',
   vatRate: 0.25,
-};
+}
 
 // mock of the user DB.
 const userDB = [
-  { id: 1, name: "Adam", email: "adam@adam.co.uk" },
-  { id: 2, name: "Dave", email: "dave@allthingsdave.com" },
-  { id: 3, name: "Laura", email: "laura@python-emporium.biz" },
-];
+  { id: 1, name: 'Adam', email: 'adam@adam.co.uk' },
+  { id: 2, name: 'Dave', email: 'dave@allthingsdave.com' },
+  { id: 3, name: 'Laura', email: 'laura@python-emporium.biz' },
+]
 
 // mock the basket DB.
 const basketDB = [
@@ -55,51 +55,51 @@ const basketDB = [
     id: 1,
     userId: 1,
     items: [
-      { id: 1, name: "Puggo", qty: 1, unitPrice: 10000 },
-      { id: 5, name: "Dog food", qty: 3, unitPrice: 250 },
+      { id: 1, name: 'Puggo', qty: 1, unitPrice: 10000 },
+      { id: 5, name: 'Dog food', qty: 3, unitPrice: 250 },
     ],
   },
   {
     id: 2,
     userId: 3,
     items: [
-      { id: 2, name: "Python", qty: 4, unitPrice: 3500 },
-      { id: 3, name: "Reptile house", qty: 2, unitPrice: 1000 },
-      { id: 8, name: "Mice", qty: 20, unitPrice: 500 },
+      { id: 2, name: 'Python', qty: 4, unitPrice: 3500 },
+      { id: 3, name: 'Reptile house', qty: 2, unitPrice: 1000 },
+      { id: 8, name: 'Mice', qty: 20, unitPrice: 500 },
     ],
   },
-];
+]
 
 const getUser = (email: string): Promise<User> => {
   try {
-    const user = userDB.find((user) => user.email === email);
+    const user = userDB.find(user => user.email === email)
     if (!user) {
-      throw Error("No user with given email");
+      throw Error('No user with given email')
     }
-    return Promise.resolve(user);
+    return Promise.resolve(user)
   } catch (error: unknown) {
     const errorMessage =
-      error instanceof Error ? error.message : "Could not fetch user";
-    return Promise.reject(errorMessage);
+      error instanceof Error ? error.message : 'Could not fetch user'
+    return Promise.reject(errorMessage)
   }
-};
+}
 
 const getBasket = (userId: number): Promise<Basket> => {
   try {
-    const basket = basketDB.find((basket) => basket.userId === userId);
+    const basket = basketDB.find(basket => basket.userId === userId)
     if (!basket) {
-      throw Error("No basket found with given user ID");
+      throw Error('No basket found with given user ID')
     }
-    return Promise.resolve(basket);
+    return Promise.resolve(basket)
   } catch (error: unknown) {
     const errorMessage =
-      error instanceof Error ? error.message : "Could not fetch basket";
-    return Promise.reject(errorMessage);
+      error instanceof Error ? error.message : 'Could not fetch basket'
+    return Promise.reject(errorMessage)
   }
-};
+}
 
-const basketContainsPug = (items: Basket["items"]) => (config: Config) =>
-  config.code === "no" && items.find((item) => item.name === "Puggo");
+const basketContainsPug = (items: Basket['items']) => (config: Config) =>
+  config.code === 'no' && items.find(item => item.name === 'Puggo')
 
 // version of validateBasket without lifting the basketContainsPug
 // into a context of ReaderEither.
@@ -116,49 +116,48 @@ const basketContainsPug = (items: Basket["items"]) => (config: Config) =>
 //   )
 
 const validateBasket = (
-  basket: Basket
+  basket: Basket,
 ): RE.ReaderEither<Config, string, Basket> =>
   pipe(
     RE.of(basketContainsPug),
     RE.ap(RE.of(basket.items)),
     RE.ap(RE.ask()),
-    RE.chain((isValid) =>
+    RE.chain(isValid =>
       isValid
-        ? RE.left("We cannot sell Puggos in your country")
-        : RE.right(basket)
-    )
-  );
+        ? RE.left('We cannot sell Puggos in your country')
+        : RE.right(basket),
+    ),
+  )
 
 const calculateTotal = (basket: Basket): R.Reader<Config, number> =>
   pipe(
     R.ask<Config>(),
-    R.map<Config, number>((config) =>
+    R.map<Config, number>(config =>
       pipe(
         basket,
-        (basket) => basket.items,
-        (items) =>
+        basket => basket.items,
+        items =>
           items.map(
-            (item) =>
-              item.unitPrice + item.unitPrice * config.vatRate * item.qty
+            item => item.unitPrice + item.unitPrice * config.vatRate * item.qty,
           ),
-        (totals) => totals.reduce((x, y) => x + y, 0)
-      )
-    )
-  );
+        totals => totals.reduce((x, y) => x + y, 0),
+      ),
+    ),
+  )
 
 const calculateBasketTotal = (email: string): any =>
   pipe(
     // fetch a user so we can get their basket.
     TE.tryCatch<string, User>(
       () => getUser(email),
-      (error) => error
+      error => error,
     ),
     // fetch a user's basket
-    TE.chain<string, User, Basket>((user) =>
+    TE.chain<string, User, Basket>(user =>
       TE.tryCatch<string, Basket>(
         () => getBasket(user.id),
-        (error) => error
-      )
+        error => error,
+      ),
     ),
     // switch to from task either to reader task either so that we can
     // "inject" our config.
@@ -166,14 +165,14 @@ const calculateBasketTotal = (email: string): any =>
     // validateBasket is not a reader task either it's a reader either
     // so we use a Kleisli chain to.
     RTE.chainReaderEitherK(validateBasket),
-    RTE.chainReaderK(calculateTotal)
-  );
+    RTE.chainReaderK(calculateTotal),
+  )
 
-calculateBasketTotal("adam@adam.co.uk")(gbConfig)().then(
+calculateBasketTotal('adam@adam.co.uk')(gbConfig)().then(
   // calculateBasketTotal('dave@allthingsdave.com')(noConfig)().then(
   // calculateBasketTotal('laura@python-emporium.biz')(noConfig)().then(
   E.fold(
-    (e) => console.log("Got an error:", e),
-    (r) => console.log("Total is:", r)
-  )
-);
+    e => console.log('Got an error:', e),
+    r => console.log('Total is:', r),
+  ),
+)
