91d71146cdbc231b4182be1cff2f16aa9136109e
adjohnston <adam.johnston3679@gmail.com>
2024-03-01 08:49:33 +0000

    boot.dev ch2.1

--- go/chirpy/main.go
+++ go/chirpy/main.go
@@ -1,14 +1,43 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
+
+type apiConfig struct {
+	fileserverHits int
+}
 
 func healthz(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("OK"))
+}
+
+func metrics(c *apiConfig) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=UTF-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(fmt.Sprintf("Hits: %d", c.fileserverHits)))
+	}
+}
+
+func resetMetrics(c *apiConfig) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		c.fileserverHits = 0
+		w.WriteHeader(http.StatusOK)
+	}
+}
+
+func middlewareMetricsInc(cfg *apiConfig, next http.Handler) http.Handler {
+	handler := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits++
+		next.ServeHTTP(w, r)
+	})
+
+	return handler
 }
 
 func middlewareCors(next http.Handler) http.Handler {
@@ -31,9 +60,14 @@ func middlewareCors(next http.Handler) http.Handler {
 func main() {
 	mux := http.NewServeMux()
 	corsMux := middlewareCors(mux)
+	hits := apiConfig{fileserverHits: 0}
 
 	mux.Handle("/healthz", http.HandlerFunc(healthz))
-	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	mux.Handle("/metrics", http.HandlerFunc(metrics(&hits)))
+	mux.Handle("/reset", http.HandlerFunc(resetMetrics(&hits)))
+
+	mux.Handle("/app", middlewareMetricsInc(&hits, http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
+
 	mux.Handle("assets/logo.png", http.FileServer(http.Dir("./assets/logo.png")))
 
 	server := &http.Server{
